4383c6b0d40568379ebc283788ecd26c70c7d1a7
7sDream <didislover@gmail.com>
2016-04-07 21:08:34 +0800

    add pypi classifiers python 2.7 and 3.5

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
