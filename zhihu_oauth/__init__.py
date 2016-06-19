5833f38b04ab373ed51e117d72417efc24f6ac13
7sDream <didislover@gmail.com>
2016-06-20 01:58:07 +0800

    update version code

--- zhihu_oauth/__init__.py
+++ zhihu_oauth/__init__.py
@@ -11,4 +11,4 @@ __all__ = ['ZhihuClient', 'ANONYMOUS', 'Activity', 'ActType', 'Article',
            'Answer', 'Collection', 'Column', 'Comment', 'People', 'Question',
            'Topic', 'NeedCaptchaException', 'UnexpectedResponseException']
 
-__version__ = '0.0.12'
+__version__ = '0.0.13'
