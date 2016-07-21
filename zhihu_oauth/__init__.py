932147102033ed52660564b2a2fa94d23e99c0be
7sDream <7seconddream@gmail.com>
2016-07-22 00:59:34 +0800

    fix issue #18, update version code to 0.0.15

--- zhihu_oauth/__init__.py
+++ zhihu_oauth/__init__.py
@@ -11,4 +11,4 @@ __all__ = ['ZhihuClient', 'ANONYMOUS', 'Activity', 'ActType', 'Article',
            'Answer', 'Collection', 'Column', 'Comment', 'People', 'Question',
            'Topic', 'NeedCaptchaException', 'UnexpectedResponseException']
 
-__version__ = '0.0.14'
+__version__ = '0.0.15'
