2dffe01280f5c8374645d22cf80ae85cd95224b5
Xiang Lv <xiang.lv@dianrong.com>
2016-08-23 17:53:32 +0800

    优化

--- user_bestanswer.py
+++ user_bestanswer.py
@@ -18,7 +18,7 @@ client.load_token('token.pkl')
 def user_bestanswers():
     database = Database()
     tx = database.graph.begin()
-    topicId = 19556664
+    topicId = 19550937
     topic = client.topic(topicId)
     answers = topic.best_answers
     i = 0
@@ -44,7 +44,7 @@ def user_bestanswers():
                         temp_education += education.school.name
                     if 'major' in education:
                         temp_education += education.major.name
-            author_education = json.dumps(temp_education.replace("\\", "").replace("'", ""))
+            author_education = json.dumps(temp_education)
 
             temp_employment = ''
             if author.employments:
@@ -53,21 +53,21 @@ def user_bestanswers():
                         temp_employment += employment.company.name
                     if 'job' in employment:
                         temp_employment += employment.job.name
-            author_employment = json.dumps(temp_employment.replace("\\", "").replace("'", ""))
+            author_employment = json.dumps(temp_employment)
 
             # 答案信息
             thanks_count = str(answer.thanks_count)
             voteup_count = str(answer.voteup_count)
             comment_count = str(answer.comment_count)
-            excerpt = json.dumps(answer.excerpt.replace("\\", "").replace("'", ""))
+            excerpt = json.dumps(answer.excerpt)
             # print(answer.excerpt.replace("\\", "").replace("'", ""))
 
             cypher = "merge(u:User{userId: '"+str(author.id)+"'}) on create set u.name='"+author_name+"',u.email='"+author_email+"',u.gender='"+author_gender+"'," \
                     "u.weibo='"+author_weibo+"', u.loation='"+author_location+"'," \
-                    "u.business='"+author_business+"',u.education='"+author_education+"'," \
-                    "u.employment='"+author_employment+"',u.topicID='"+str(topicId)+"'"
+                    "u.business='"+author_business+"',u.education="+author_education+"," \
+                    "u.employment="+author_employment+",u.topicID='"+str(topicId)+"'"
             tx.run(cypher)
-            relationShip = "match(u:User{userId: '"+str(author.id)+"'}) MERGE (u)-[:AUTHOR]->(a:Answer{answerId:'"+str(answer.id)+"'}) on create set a.excerpt='"+excerpt+"'," \
+            relationShip = "match(u:User{userId: '"+str(author.id)+"'}) MERGE (u)-[:AUTHOR]->(a:Answer{answerId:'"+str(answer.id)+"'}) on create set a.excerpt="+excerpt+"," \
                             "a.thanks_count="+thanks_count+",a.voteup_count="+voteup_count+"," \
                             "a.comment_count="+comment_count+",a.question='"+answer.question.title+"'"
             tx.run(relationShip)
---
+++ user_followers.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+import sys
+import json
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+
+from zhihu_oauth import ZhihuClient
+from neo4j import Database
+
+client = ZhihuClient()
+client.load_token('token.pkl')
+database = Database()
+
+# me = client.me()
+
+def user_bestanswers():
+
+    userIDs = database.graph.data("match(u:User{topicID:'19554298'}) return u.userId as userId ")
+    for userId in userIDs:
+        people = client.people(userId["userId"])
+        try:
+            for follower in people.followings:
+                try:
+                    insertNeo4j(follower, people.id)
+                    print("一度关系成功")
+                except Exception, e:
+                    print(e)
+                    failure = database.graph.begin()
+                    failure.run("create(f:UserFailure{id:'"+str(follower.id)+"'})")
+                    failure.commit()
+                    continue
+                for thirdFollow in follower.followings:
+                    try:
+                        insertNeo4j(thirdFollow, follower.id)
+                        print("二度关系成功")
+                    except Exception, e:
+                        print(e)
+                        failure = database.graph.begin()
+                        failure.run("create(f:UserFailure{id:'"+str(thirdFollow.id)+"'})")
+                        failure.commit()
+                        continue
+        except Exception, e:
+            print(e.message)
+            continue
+    print("it is over")
+
+
+def insertNeo4j(follower, userId):
+    tx = database.graph.begin()
+    author = userinfo(follower)
+    cypher = "merge(u:User{userId: '"+author["author_id"]+"'}) on create set u.name='"+author["author_name"]+"',u.email='"+author["author_email"]+"',u.gender='"+author["author_gender"]+"'," \
+                    "u.weibo='"+author["author_weibo"]+"', u.loation='"+author["author_location"]+"'," \
+                    "u.business='"+author["author_business"]+"',u.education="+author["author_education"]+"," \
+                    "u.employment="+author["author_employment"]+""
+    tx.run(cypher)
+    userRelations = "match(u:User{userId :'"+str(userId)+"'}) with u " \
+                    "match(au:User{userId:'"+author["author_id"]+"'}) merge(u)-[:FOLLOWING]->(au)"
+    tx.run(userRelations)
+                # 回答相关
+    follower_answers = follower.answers
+    for answer in follower_answers:
+        myanswer = user_answer(answer)
+        relationShip = "match(u:User{userId: '"+author["author_id"]+"'}) MERGE (u)-[:AUTHOR]->(a:Answer{answerId:'"+myanswer["answer_id"]+"'}) on create set a.excerpt="+myanswer["excerpt"]+"," \
+                            "a.thanks_count="+myanswer["thanks_count"]+",a.voteup_count="+myanswer["voteup_count"]+"," \
+                            "a.comment_count="+myanswer["comment_count"]+",a.question="+myanswer["title"]+""
+        tx.run(relationShip)
+    tx.commit()
+
+
+def userinfo(author):
+    peopleInfo = {}
+    peopleInfo["author_name"] = author.name
+    peopleInfo["author_weibo"] = author.sina_weibo_url if author.sina_weibo_url else ''
+    peopleInfo["author_email"] = author.email if author.email else ''
+    temp_location = ''
+    if author.locations:
+        for location in author.locations:
+            temp_location += location.name
+    peopleInfo["author_location"] = temp_location
+    peopleInfo["author_gender"] = "male" if author.gender == 1 else "female" if author.gender == 2 else "未填"
+    peopleInfo["author_business"] = author.business.name if author.business else ""
+
+    temp_education = ''
+    if author.educations:
+        for education in author.educations:
+            if 'school' in education:
+                temp_education += education.school.name
+            if 'major' in education:
+                temp_education += education.major.name
+    peopleInfo["author_education"] = json.dumps(temp_education)
+
+    temp_employment = ''
+    if author.employments:
+        for employment in author.employments:
+            if 'company' in employment:
+                temp_employment += employment.company.name
+            if 'job' in employment:
+                temp_employment += employment.job.name
+    peopleInfo["author_employment"] = json.dumps(temp_employment)
+    peopleInfo["author_id"] = str(author.id)
+    return peopleInfo
+
+def user_answer(answer):
+    answerInfo = {}
+    answerInfo["thanks_count"] = str(answer.thanks_count)
+    answerInfo["voteup_count"] = str(answer.voteup_count)
+    answerInfo["comment_count"] = str(answer.comment_count)
+    answerInfo["excerpt"] = json.dumps(answer.excerpt)
+    answerInfo["answer_id"] = str(answer.id)
+    answerInfo["title"] = json.dumps(answer.question.title)
+    return answerInfo
+
+def main():
+    user_bestanswers()
+
+
+if __name__ == '__main__':
+    main()
