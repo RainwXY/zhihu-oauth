bb89a6f40ac6677d80efc6b08b2d9edbebeacc4f
Xiang Lv <xiang.lv@dianrong.com>
2016-08-25 20:55:36 +0800

    旅游

--- user_followers.py
+++ user_followers.py
@@ -18,7 +18,7 @@ database = Database()
 
 def user_bestanswers():
 
-    userIDs = database.graph.data("match(u:User{topicID:'19554298'}) where u.name<>'匿名用户' and u.grab is null return u.userId as userId order by id(u) asc skip 0 limit 300")
+    userIDs = database.graph.data("match(u:User{topicID:'19554298'}) where u.name<>'匿名用户' and u.grab is null return u.userId as userId order by id(u) asc skip 300 limit 300")
     for userId in userIDs:
         people = client.people(userId["userId"])
         try:
