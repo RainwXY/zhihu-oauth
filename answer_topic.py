9cacbe5f2dcf246b4ad3431b089dd44c27c669aa
Xiang Lv <xiang.lv@dianrong.com>
2016-08-30 22:09:09 +0800

    40

--- answer_topic.py
+++ answer_topic.py
@@ -16,7 +16,7 @@ database = Database()
 
 def user_bestanswers():
 
-    i = 457832
+    i = 226714
     j = 0
     while True:
         answerIDs = database.graph.data("match(u:User)-[:AUTHOR]->(a:Answer) return a.answerId as answerId skip "+ str(i) +" limit 100")
@@ -40,7 +40,7 @@ def user_bestanswers():
             except Exception, e:
                 print(e)
                 continue
-        if i > 600000:
+        if i > 400000:
             break
         i += 100
         print("抓取了"+str(i)+"answer->topic")
