c78d26dc374e77a0aa09afab81ad362152ceb709
Xiang Lv <xiang.lv@dianrong.com>
2016-09-16 21:21:55 +0800

    oliver

--- neo4jA.py
+++ neo4jA.py
@@ -5,5 +5,5 @@ class Database:
     graph = None
 
     def __init__(self):
-        self.graph = Graph(bolt=False, host="120.27.42.59:32769", http_port=32769, user="neo4j", password="admin")
+        self.graph = Graph(bolt=False, host="120.27.42.59", http_port=32769, user="neo4j", password="admin")
         # graph = Graph(bolt=True, host="localhost", password="admin")
